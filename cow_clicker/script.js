cedc51333f5982827ad9ce49f2df576e682ce5f1
AjobK <ajobkustra.p@gmail.com>
2021-11-12 23:18:01 +0100

    keep clicks

--- cow_clicker/script.js
+++ cow_clicker/script.js
@@ -1,9 +1,14 @@
 var clicker = document.getElementById("cow")
+var cow_counter = localStorage.getItem("cow_counter") ? localStorage.getItem("cow_counter")*1 : 0
+
 var cow_counter_text = document.getElementById("teller")
-var cow_counter = 0
+cow_counter_text.innerHTML = cow_counter
+
 
 function add_cow() {
     cow_counter++
+    localStorage.setItem("cow_counter", cow_counter)
+
     cow_counter_text.innerHTML = cow_counter
 }
 
