7ada90d4aa9f0a735de2468b8fcd0d559cbc5cfc
AjobK <ajobkustra.p@gmail.com>
2021-11-14 21:26:44 +0100

    shop in progreesssssssssss

--- cow_clicker/script.js
+++ cow_clicker/script.js
@@ -1,5 +1,17 @@
 var clicker = document.getElementById("cow")
 var cow_counter = localStorage.getItem("cow_counter") ? localStorage.getItem("cow_counter")*1 : 0
+var shop_items = [
+    {
+        name: "Baguette",
+        image: "./images/shop_items/baguette.png",
+        price: 40
+    },
+    {
+        name: "Apple",
+        image: "./images/shop_items/apple.png",
+        price: 150
+    }
+]
 
 var cow_counter_text = document.getElementById("counter")
 cow_counter_text.innerText = cow_counter
@@ -13,4 +25,14 @@ function add_cow() {
 }
 
 clicker.addEventListener('click', add_cow)
+
+shop_items.forEach(create_product_element)
+
+function create_product_element(product) {
+    var para = document.createElement("p")
+    para.innerText = product.name
+    var element = document.getElementById("shop")
+    element.appendChild(para)
+    console.log(product)
+}
 
