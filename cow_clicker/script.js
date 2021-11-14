da1d56af2b4aa292a0b6e90f668b86d75e114e44
AjobK <ajobkustra.p@gmail.com>
2021-11-14 20:08:30 +0100

    shop and purchased border

--- cow_clicker/script.js
+++ cow_clicker/script.js
@@ -1,15 +1,15 @@
 var clicker = document.getElementById("cow")
 var cow_counter = localStorage.getItem("cow_counter") ? localStorage.getItem("cow_counter")*1 : 0
 
-var cow_counter_text = document.getElementById("teller")
-cow_counter_text.innerHTML = cow_counter
+var cow_counter_text = document.getElementById("counter")
+cow_counter_text.innerText = cow_counter
 
 
 function add_cow() {
     cow_counter++
     localStorage.setItem("cow_counter", cow_counter)
 
-    cow_counter_text.innerHTML = cow_counter
+    cow_counter_text.innerText = cow_counter
 }
 
 clicker.addEventListener('click', add_cow)
