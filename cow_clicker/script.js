94ebecc234dcec61d076a6a807c5dcb313ba413e
Danique Wolters <danique-wolters@hotmail.com>
2021-11-15 20:27:22 +0100

    shop updated

--- cow_clicker/script.js
+++ cow_clicker/script.js
@@ -9,7 +9,7 @@ var shop_items = [
     {
         name: "Apple",
         image: "./images/shop_items/apple.png",
-        price: 150
+        price: 150 
     }
 ]
 
@@ -17,22 +17,30 @@ var cow_counter_text = document.getElementById("counter")
 cow_counter_text.innerText = cow_counter
 
 
-function add_cow() {
-    cow_counter++
+function add_cow(amount) {
+    cow_counter+=amount
     localStorage.setItem("cow_counter", cow_counter)
 
     cow_counter_text.innerText = cow_counter
 }
 
-clicker.addEventListener('click', add_cow)
+clicker.addEventListener('click', () => add_cow(1))
 
 shop_items.forEach(create_product_element)
 
 function create_product_element(product) {
+    var wrapper = document.createElement("div")
     var para = document.createElement("p")
+    var pic = document.createElement("img")
+    var price = document.createElement("p")
+    wrapper.classList.add("product")
     para.innerText = product.name
+    pic.src = product.image
+    pic.alt = product.name
+    price.innerText = product.price + " clicks"
     var element = document.getElementById("shop")
-    element.appendChild(para)
+    wrapper.append(para, pic, price)
+    element.appendChild(wrapper)
     console.log(product)
 }
 
